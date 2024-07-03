c4c2146beb82dec77fedbff76658049610c1dfc8
Anton7705 <167810780+Anton7705@users.noreply.github.com>
2024-07-03 21:21:44 +0300

    7-2

--- ru/dorogov/mathematics/Сalculations.java
+++ ru/dorogov/mathematics/Сalculations.java
@@ -1,5 +1,7 @@
 package ru.dorogov.mathematics;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.pow;
 
 //4.1.3
 public class Сalculations {
@@ -13,9 +15,9 @@ public class Сalculations {
     }
 //4.1.4
     public static double exponentiation(String x, String y) {
-        int x1 = Integer.parseInt(x);
-        int y1 = Integer.parseInt(y);
-        return Math.pow(x1,y1);
+        int x1 = parseInt(x);
+        int y1 = parseInt(y);
+        return pow(x1,y1);
     }
 }
 
